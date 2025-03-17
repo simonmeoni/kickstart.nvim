cd3f328e6cbd7d06828f0977cbecf4e9d4eb8037
Simon Meoni <simonmeoni@aol.com>
2025-03-17 18:46:25 +0100

    add config to undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -2,8 +2,6 @@ return {
   'mbbill/undotree',
   config = function()
     vim.cmd [[
-      let g:undotree_WindowLayout = 3
-      let g:undotree_DiffpanelHeight = 10
       if has("persistent_undo")
         let target_path = expand('~/.undodir')
         if !isdirectory(target_path)
@@ -12,6 +10,8 @@ return {
         let &undodir = target_path
         set undofile
       endif
+      let g:undotree_WindowLayout = 4
+      let g:undotree_SetFocusWhenToggle = 1
     ]]
     vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, {})
   end,
